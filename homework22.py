0e605b501114ef13cedcf2fc90ded002be80165e
ls25047 <ls_25047@mail.ru>
2022-12-03 07:08:13 +0300

    QWEQWE

---
+++ homework22.py
@@ -0,0 +1,22 @@
+a = float(input('input firstnumber as a here please:'))
+b = float(input('input secondnumber as b here please:'))
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a * b = {a * b}')
+print(f'a / b = {a / b}')
+print(f'a^b = {a ** b}')
+print(f'ostatok ot delenija a na b = {a % b}')
+print(f'celoe a na b = {a + b}')
+
+
+Name = input('May i know your name ?')
+print(f'nice to meet you {Name}')
+
+
+word = input("input you string don't forget that there are more than two characters in our string c; =>")
+print(word[-1] + word[-2])
+
+
+r = float(input('put your radius here ->'))
+from math import pi 
+print(f'S kryga = {pi * r**2 }')
